de36ef6d1cee9888f84160401138dc23b4fc4879
Jeriramirez29 <142386443+Jeriramirez29@users.noreply.github.com>
2024-01-22 18:29:35 +0100

    Insert the Javascript needed to automatically tell time

---
+++ java/index.js
@@ -0,0 +1,22 @@
+function updateTime() {
+//Los Angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML = losAngelesCurrentTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesCurrentTime.format("h:mm:ss [<small>]A[</small]");
+
+//Paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisCurrentTime = moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML = parisCurrentTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = parisCurrentTime.format("h:mm:ss [<small>]A[</small]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
